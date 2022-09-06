dbe3573c4a1d10d5818d0f4a31268d648231cb6c
MacBook Air Oskr <oscarvarelagallardo@MacBook-Air-de-Oskr.local>
2022-09-06 13:04:57 -0500

    Paso dos

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
+
 const btnagregarContacto = document.getElementById("btnAgregarContacto");
 const btnbuscarContacto = document.getElementById("btnbuscarContactos");
-const btnmostrarContactos = document.getElementById("btnMostrarContactos");
+const btnMostrarContactos = document.getElementById("btnMostrarContactos");
 const btnCerrar = document.getElementById("btnCerrar");
 const formAdd = document.getElementById("addNewContact");
 const formBuscar = document.getElementById("lfContacts");
@@ -30,8 +31,9 @@ btnbuscarContacto.addEventListener("click", () => {
 
 
 
-btnagregarContacto.addEventListener('click', (e) => {
+btnagregarContacto.addEventListener('click', () => {
 
+   
     formAdd.style.display = "block";
     formAdd.style.visibility = "visible";
     formBuscar.style.visibility = "hidden";
@@ -39,73 +41,71 @@ btnagregarContacto.addEventListener('click', (e) => {
     formBuscar.style.display = "none";
     formMostrar.style.display = "none"
 
+    guardarContacto.addEventListener("click", () => {
 
 
-    guardarContacto.addEventListener("click", (e) => {
         let nombre = document.getElementById("nombre").value;
         let telefono = document.getElementById("telefono").value;
         let email = document.getElementById("email").value;
-        let dbcontactos=localStorage.getItem("contactos")
+        let dbcontactos = localStorage.getItem("contactos")
         let contactos = JSON.parse(dbcontactos)
-        if (contactos == null) {
+        if(nombre==""&& telefono==""&& email==""){
+             return alert("Por favor ingrese todos los datos")
+
+        }if(contactos == null) {
             contactos = []
-        }contactos.push({nombre, telefono, email})
+        } contactos.push({ nombre, telefono, email })
         localStorage.setItem("contactos", JSON.stringify(contactos))
-        console.log(contactos)  
-
-
-
-    })
-
-
-});
-
-btnmostrarContactos.addEventListener('click', (e) => {
-
-    formMostrar.style.display = "block";
-    formMostrar.style.visibility = "visible";
-    formBuscar.style.visibility = "hidden";
-    formAdd.style.visibility = "hidden";
-    formBuscar.style.display = "none";
-    formAdd.style.display = "none";
-    let dbcontactos=localStorage.getItem("contactos")
-    let contactos = JSON.parse(dbcontactos)
-    if (contactos == null) {
-        contactos = []
-    }
-    let mostrarContactos = document.getElementById("table")
-    for(let i=0; i<contactos.length; i++){
-        let fila = mostrarContactos.insertRow(1)
-        let celdaNombre = fila.insertCell(0)
-        let celdaTelefono = fila.insertCell(1)
-        let celdaEmail = fila.insertCell(2)
-        celdaNombre.innerHTML = contactos[i].nombre
-        celdaTelefono.innerHTML = contactos[i].telefono
-        celdaEmail.innerHTML = contactos[i].email
-        //botones
-        let celdaBoton = fila.insertCell(3)
-        let btnEditar = document.createElement("button")
-        btnEditar.innerHTML = "Editar"
-        btnEditar.setAttribute("class", "btn btn-warning")
-        btnEditar.setAttribute("id", "btnEditar")
-        celdaBoton.appendChild(btnEditar)
-        let celdaBoton2 = fila.insertCell(3)
-        let btnEliminar = document.createElement("button")
-        btnEliminar.innerHTML = "Eliminar"
-        btnEliminar.setAttribute("class", "btn btn-danger")
-        btnEliminar.setAttribute("id", "btnEliminar")
-        celdaBoton2.appendChild(btnEliminar)
-
-    }
-
-
-
-});
-
-btnCerrar.addEventListener('click', (e) => {
-
-    formMostrar.style.display = "none";
-    formBuscar.style.display = "none";
-    formAdd.style.display = "none";
-
+        console.log(contactos)
+        swal("Contacto agregado")
+    });
 })
+
+    btnMostrarContactos.addEventListener("click", () => {
+
+            formMostrar.style.display = "block";
+            formMostrar.style.visibility = "visible";
+            formBuscar.style.visibility = "hidden";
+            formAdd.style.visibility = "hidden";
+            formBuscar.style.display = "none";
+            formAdd.style.display = "none";
+            let dbcontactos = localStorage.getItem("contactos")
+            let contactos = JSON.parse(dbcontactos)
+            if (contactos == null) {
+                contactos = []
+            }
+          //  let showContacts = document.getElementById("showContacts")
+           // showContacts.innerHTML = ""
+
+            let table = document.getElementById("table")
+            table.innerHTML = ""
+           
+            for (let i = 0; i < contactos.length; i++) {
+                let nombre = contactos[i].nombre
+                let telefono = contactos[i].telefono
+                let email = contactos[i].email
+                table.innerHTML += `
+                <tr>
+                <th>Nombre</th>
+                <th>Telefono</th>
+                <th>Email</th>
+                <th>Acciones</th>
+                
+                </tr>
+                
+                <tr>
+                <td>${nombre}</td>
+                <td>${telefono}</td>
+                <td>${email}</td>
+                <td>
+                <button class="btn btn-success" onclick="editarContacto(${i})">Editar</button>
+
+                <button class="btn btn-danger" onclick="eliminarContacto(${i})">Eliminar</button></td>
+
+                </tr>` 
+
+
+            }
+        })  
+            
+           
